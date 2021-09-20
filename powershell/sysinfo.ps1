d8fc78512d08754a9e098b33954c28e949347db4
Rohan Patel <patel4r4@mail.uc.edu>
2021-09-20 14:37:36 -0400

    Update sysinfo.ps1

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -11,8 +11,8 @@ $IP = getIP
 $USER = $env:USERNAME
 $DATE = Get-Date -DisplayHint Date
 $HOSTNAME = $env:COMPUTERNAME
-
-$BODY = "This machine's IP is $IP. User is $USER. Hostname is $HOSTNAME. Today's Date is $DATE"
-Send-MailMessage -To "rohanpatelxf@gmail.com" -From "rohanpatelxf@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential) 
+$HOST = Get-Host | Select-Object Version 
+$BODY = "This machine's IP is $IP. User is $USER. Hostname is $HOSTNAME.Powershell Version $HOST. Today's Date is $DATE"
+##Send-MailMessage -To "rohanpatelxf@gmail.com" -From "rohanpatelxf@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential) 
 ##botheaj@ucmail.uc.edu
 write-host($BODY)
