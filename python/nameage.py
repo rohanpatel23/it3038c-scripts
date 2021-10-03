304236f7e0e05ef65d8dcd5e3138ec6d3e0e4ed5
Rohan Patel <patel4r4@mail.uc.edu>
2021-10-03 19:09:15 -0400

    New Python script

---
+++ python/nameage.py
@@ -0,0 +1,10 @@
+print('What is your name?') 
+
+myName = input() 
+
+print('Hello, '+ myName + '. That is a good name. How old are you?') 
+
+myAge = input() 
+
+print(myAge + '? That’s funny, Im only a few seconds old.') 
+print("I wish I was " + str(int(myAge) * 2) + " years old")   
